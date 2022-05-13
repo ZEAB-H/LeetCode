8c2a0fd8a2b46c0b14974c9378df36d0b3987c9b
abeldejene30 <96728745+ZEAB-H@users.noreply.github.com>
2022-05-13 20:07:51 +0300

    Time: 56 ms (72.99%), Space: 15.2 MB (11.92%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+        anagram1 = set(s)
+        anagram2 = set(t)
+        
+        for i in anagram1:
+            if anagram1 !=anagram2:
+               return False
+        return True
